ca45a0adeda2422034c0f005a2ff4479b0aa5052
Giacomo Barcherini <>
2026-05-06 21:18:24 +0200

    feat: upgrade vite-plugin-pwa to v1.3.0 and implement service worker registration logic

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,16 @@
 import './style.css';
 import './components/theme-picker/theme-picker.component';
 import './app/app.component';
+
+import { registerSW } from 'virtual:pwa-register';
+
+registerSW({
+  onNeedRefresh() {
+    if (confirm('New content available. Reload?')) {
+      window.location.reload();
+    }
+  },
+  onOfflineReady() {
+    console.log('App ready to work offline.');
+  },
+});
